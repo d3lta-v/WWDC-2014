6ff139ce0b5e6f72cdc4c8750681724985988d81
Pan Ziyue <random.rrr3@gmail.com>
2014-04-08 13:13:05 +0800

    patched up some animation bugs

--- Pan Ziyue/ViewController.m
+++ Pan Ziyue/ViewController.m
@@ -29,32 +29,22 @@
     hiLabel.alpha=1;
     [UIView setAnimationDidStopSelector:@selector(animationDidStop:finished:)];
     [UIView commitAnimations];
-    
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        [UIView beginAnimations:nil context:NULL];
-        [UIView setAnimationDelegate:self];
-        [UIView setAnimationCurve:UIViewAnimationCurveEaseIn];
-        [UIView setAnimationDuration:1];
-        ziyueLabel.alpha=1;
-        [UIView setAnimationDidStopSelector:@selector(animationDidStop:finished:)];
-        [UIView commitAnimations];
-    });
 }
 
 - (void)animationDidStop:(CAAnimation *)theAnimation finished:(BOOL)flag
 {
-    //[UIView beginAnimations:nil context:nil];
+    [UIView beginAnimations:nil context:NULL];
+    [UIView setAnimationDelegate:self];
+    [UIView setAnimationCurve:UIViewAnimationCurveEaseIn];
+    [UIView setAnimationDuration:1];
+    ziyueLabel.alpha=1;
+    [UIView setAnimationDidStopSelector:@selector(animation2DidStop:finished:)];
+    [UIView commitAnimations];
+}
+
+-(void)animation2DidStop:(CAAnimation *)anim finished:(BOOL)flag
+{
     
-    //hiLabel.alpha=1.0f;
-    
-    //feed.alpha = 1.0;
-    //NSString *nextFeed = [self getNextFeed]; // Need to implement getNextFeed
-    //if (nextFeed)
-    //{
-        // Only continue if there is a next feed.
-        //[feed setText:nextFeed];
-        //[self performAnimation];
-    //}
 }
 
 - (void)didReceiveMemoryWarning
